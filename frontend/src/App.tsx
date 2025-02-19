0ae45afc86aae100f7431de0c6f9756849a21538
oderflaY <152356284+oderflaY@users.noreply.github.com>
2025-02-19 13:37:52 -0600

    Nuevo orden
    Nuevo acomodo del sistema de rutas y carpetas y cambios minimos

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+
+import './styles/App.css'
+
+function App() {
+
+const holamundo = "hola mundo xd"
+
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline bg-amber-600 " >hola munod</h1>
+    {holamundo}
+    </>
+  )
+}
+
+export default App
