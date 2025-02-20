4ab3d24dfc7c33f2205cef42e4cd581666ba9b8c
oderflaY <152356284+oderflaY@users.noreply.github.com>
2025-02-20 10:11:25 -0600

    Login estetico

--- frontend/src/views/LoginView.tsx
+++ frontend/src/views/LoginView.tsx
@@ -1,15 +1,25 @@
 import { Link } from "react-router-dom"
-
+import '../styles/App.css'
 export default function LoginView() {
   return (
     <>
-    
-    <div className="text-6xl">LoginView</div>
-    <nav>
+    <div className="flex h-screen">
+  <div className="w-1/2 min-h-screen">
+  <img></img>
+  <nav>
+
         <Link to="/auth/register">
             No tienes una cuenta? crea una aqi
         </Link>
+
     </nav>
+  
+  </div>
+  <div
+    className="w-1/2 min-h-screen bg-cover bg-center"
+    style={{ backgroundImage: "url('/public/FondoLogin.png')" }}
+  ></div>
+</div>
     </>
   )
 }
--- frontend/src/views/RegisterView.tsx
+++ frontend/src/views/RegisterView.tsx
@@ -1,11 +1,8 @@
-import React from 'react'
+
 import { Link } from 'react-router-dom'
 export default function RegisterView() {
   return (
     <>
-    <div>
-      <h1>hola puta</h1>
-    </div>
     <nav>
         <Link to="/auth/login">
             Ya tienes cuenta? Inicias Sesion
