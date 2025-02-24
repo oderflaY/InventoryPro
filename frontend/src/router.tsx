ca480194a7d9774c328eb23005142875fc850e05
Arreolax <ArreolaxStudios@hotmail.com>
2025-02-24 14:44:43 -0600

    ya no se ve el login feo (tanto)
    sapalapapipa

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -6,14 +6,16 @@ import AuthLayout from "./layouts/AuthLayout";
 
 export default function Router() {
     return (
-    <BrowserRouter>
-        <Routes>
-        <Route element={<AuthLayout />}>
-            <Route path="/auth/login" element={<LoginView />} />
-            <Route path="/auth/register" element={<RegisterView />} />
-            <Route path="/auth/homepage" element={<Homepage />} />
-        </Route>
-        </Routes>
-    </BrowserRouter>
+        <BrowserRouter>
+            <Routes>
+                <Route element={<AuthLayout />}>
+                    <Route path="/auth/login" element={<LoginView />} />
+                    <Route path="/auth/register" element={<RegisterView />} />
+                </Route>
+            </Routes>
+            <Routes>
+                <Route path="/auth/homepage" element={<Homepage />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
